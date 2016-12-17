5840b100e1186eaa38499510764bb96779eef215
Staffan Gustafsson <info@powercode-consulting.se>
2016-12-18 00:38:26 +0100

    match, in, contains

--- operators.ps1
+++ operators.ps1
@@ -1,24 +1,42 @@
-
-10 -eq 10
-
-10 -ne 10
-
-5 -gt 10
-
-5 -gt "6"
-
-"abc" -gt "bcd"
-
-11 -ge 10
-
-#bitwize
-$blocks = {0x10 -bAND 0x110}, {0xf -bOR 0x8}, {0xF -bXOR 0xc},{-bNOT 0x0}
-foreach($sb in $blocks) {
-    $result = & $sb
-    [pscustomobject] @{
-        Expression = [string] $sb
-        Binary = [Convert]::ToString($result,2)
-        Decimal = [Convert]::ToString($result,10)
-        Hex = '0x' + [Convert]::ToString($result,16)
+function show-result($sb){"$sb = $(&$sb)"}
+function show-intresult([scriptblock[]]$sb) {
+    foreach($s in $sb){
+        $result = & $s
+        [pscustomobject] @{
+            Expression = [string] $s
+            Binary = [Convert]::ToString($result,2)
+            Decimal = [Convert]::ToString($result,10)
+            Hex = '0x' + [Convert]::ToString($result,16)
+        }
     }
 }
+
+show-result { 10 -eq 10}
+
+show-result { 10 -ne 10 }
+
+show-result { 5 -gt 10 }
+
+show-result { 5 -gt "6" }
+
+show-result { "abc" -gt "bcd" }
+
+show-result { 11 -ge 10 }
+
+
+
+#logical
+show-result { $true -and $false  }
+show-result { $true -or $false   }
+show-result { -not $true         }
+show-result { ! $false           }
+
+
+#bitwize
+show-intresult { 0x10 -bAND 0x110 }, { 0xf -bOR 0x8 }, { 0xF -bXOR 0xc },{ -bNOT 0x0 }
+
+
+# -match, -notmatch, -like, -notlike
+'aaabbbccc' -match '(b+)(?<someCs>\w+)'
+
+$matches
